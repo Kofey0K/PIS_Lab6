ea358f5d004ec35e226e4a7a9ffc3e0e930ddf43
Kofey0K <56653351+Kofey0K@users.noreply.github.com>
2021-05-25 00:18:38 +0300

    Add files via upload

--- httpServer_v2.js
+++ httpServer_v2.js
@@ -1,13 +1,14 @@
 const http = require('http')
 const server = http.createServer((req, res)=>{
-    console.log(req.url);
-    if(req.url == "/is-91-018"){
-        
-        res.write("<h2>Мінін</h2><h2>Павло</h2><h2>2 курс</h2><h2>ІС-91</h2>");
+    if(req.url=="/is-91-018"){
+        res.write("Minin Pavlo 2 Year IS-91");
     }
+    else{
+        res.write("Wrong login or password");
+    }
+    res.end()
 })
 
-server.listen( 3001, () => {
-    
+server.listen( 3000, () => {
     console.log('Server has been started...')
 })
